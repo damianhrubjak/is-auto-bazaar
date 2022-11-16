3f7881702b2c06959aaa5ddbbbe94da59b8ef671
Damián Hrubják <hrubjak.damian@gmail.com>
2022-11-16 20:23:49 +0100

    Added Report page

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,15 +1,30 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "../pages/Home";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+
+import NavBar from "@/components/NavBar";
+import AppLayout from "@/layouts/AppLayout";
+import Home from "@/pages/Home";
+import Report from "@/pages/Report";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
+    {
+        path: "/",
+        element: (
+            <>
+                <NavBar />
+                <AppLayout>
+                    <Outlet />
+                </AppLayout>
+            </>
+        ),
+        children: [
+            { index: true, element: <Home /> },
+            { path: "report", element: <Report /> },
+        ],
+    },
 ]);
 
 function Router() {
-  return <RouterProvider router={router} />;
+    return <RouterProvider router={router} />;
 }
 
 export default Router;
