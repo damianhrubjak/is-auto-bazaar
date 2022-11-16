04755e81d4224c26fc50fe342d7bb9106a2c6c57
Damián Hrubják <hrubjak.damian@gmail.com>
2022-11-16 21:05:09 +0100

    Set object as server output

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1,37 +1,41 @@
-import { getConnection, initOracleClient } from "oracledb";
 import * as dotenv from "dotenv";
+import oracleDB from "oracledb";
 
 // get .env config
 dotenv.config({ path: "../.env" });
 
 class Database {
-  #connectionConfig: object;
+    #connectionConfig: object;
 
-  constructor() {
-    this.#connectionConfig = {
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      connectString: process.env.DB_CONNECTION_STRING_NAME,
-    };
+    constructor() {
+        this.#connectionConfig = {
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            connectString: process.env.DB_CONNECTION_STRING_NAME,
+        };
 
-    try {
-      // initialize oracle's JAVA client
-      initOracleClient({ libDir: "./instantclient_21_7" });
-    } catch (error) {
-      console.warn("Error has ocurred during oracle's client initialization!");
-      console.error(error);
+        oracleDB.outFormat = oracleDB.OUT_FORMAT_OBJECT;
+
+        try {
+            // initialize oracle's JAVA client
+            oracleDB.initOracleClient({ libDir: "./instantclient_21_7" });
+        } catch (error) {
+            console.warn(
+                "Error has ocurred during oracle's client initialization!"
+            );
+            console.error(error);
+        }
     }
-  }
 
-  async getConnection() {
-    try {
-      // connect to server and return connection
-      return await getConnection(this.#connectionConfig);
-    } catch (error) {
-      console.warn("Can't connect to Oracle DB server");
-      console.error(error);
+    async getConnection() {
+        try {
+            // connect to server and return connection
+            return await oracleDB.getConnection(this.#connectionConfig);
+        } catch (error) {
+            console.warn("Can't connect to Oracle DB server");
+            console.error(error);
+        }
     }
-  }
 }
 
 export default new Database();
