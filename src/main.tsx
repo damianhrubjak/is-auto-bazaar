8d2d681072a16906a83de775478a163a6b95171c
Damián Hrubják <hrubjak.damian@gmail.com>
2022-11-17 14:52:24 +0100

    Added react-query

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,20 @@
 import { StrictMode } from "react";
+
 import ReactDOM from "react-dom/client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import Router from "@/router";
 
 import "./index.pcss";
 
+// Create a client
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <StrictMode>
-    <Router />
-  </StrictMode>
+    <StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <Router />
+        </QueryClientProvider>
+    </StrictMode>
 );
